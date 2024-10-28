87288949c52439189be3c5d707b6c8473c3897c6
Iam987 <iancrittenden@rocketmail.com>
2024-10-28 15:59:12 -0600

    End of Class

--- src/StackAndQueueDemo.java
+++ src/StackAndQueueDemo.java
@@ -1,4 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
+import java.util.PriorityQueue;
 
 public class StackAndQueueDemo {
 
@@ -15,6 +18,27 @@ public class StackAndQueueDemo {
 		System.out.println(stack.pop());
 		System.out.println(stack.pop());
 		System.out.println(stack.pop());
+		
+		Queue<String> queue = new LinkedList<String>();
+		queue.add("xxx");
+		queue.add("yyy");
+		queue.add("zzz");
+		
+		System.out.println(queue.peek());
+		System.out.println(queue.poll());
+		System.out.println(queue.remove());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(40);
+		pq.add(4);
+		pq.add(76);
+		pq.add(233);
+		pq.add(10);
+		pq.add(1);
+		
+		System.out.println(pq.peek());
+		System.out.println(pq.remove());
+		System.out.println(pq.poll());
 	}
 
 }
